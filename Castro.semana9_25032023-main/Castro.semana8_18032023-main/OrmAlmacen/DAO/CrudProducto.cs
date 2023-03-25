ad34f5734051fee29be451823d4a134cdee798fd
CastroFR <kevinfrancisco906@gmail.com>
2023-03-25 00:59:54 -0600

    REPO

---
+++ Castro.semana9_25032023-main/Castro.semana8_18032023-main/OrmAlmacen/DAO/CrudProducto.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrmAlmacen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmAlmacen.DAO
+{
+    public class CrudProducto
+    {
+        public void AgregarProducto(Producto producto)
+        {
+            using (OrmAlmacenContext db = new OrmAlmacenContext())
+            {
+                Producto producto1 = new Producto();
+                producto1.Nombre = producto.Nombre;
+                producto1.Descipción = producto.Descipción;
+                producto1.Precio = producto1.Precio;
+                producto1.Stock = producto1.Stock;
+
+                db.Add(producto1);
+                db.SaveChanges();
+            }
+        }
+        public Producto ProductoUnitario(int id)
+        {
+            using (OrmAlmacenContext db = new OrmAlmacenContext())
+            {
+                var buscar = db.Productos.FirstOrDefault(s=>s.Id==id);
+                return buscar;
+
+
+            }
+
+        }
+
+        public void ActualizarProducto(Producto producto)
+        {
+            using (OrmAlmacenContext db = new OrmAlmacenContext())
+            {
+                var buscar = ProductoUnitario(producto.Id);
+                if (buscar == null) 
+                {
+                    Console.WriteLine("Id no existe");
+                }
+                else
+                {
+                    buscar.Nombre = producto.Nombre;
+                    db.Update(buscar);
+                    db.SaveChanges();
+                }
+            }
+        }
+        
+    }
+}
---
+++ Castro.semana9_25032023-main/Castro.semana8_18032023-main/OrmAlmacen/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using OrmAlmacen.DAO;
+using OrmAlmacen.Models;
+using System;
+using System.Diagnostics;
+
+CrudProducto crudProducto  = new CrudProducto();
+Producto producto = new Producto();
+Console.WriteLine("Menu");
+Console.WriteLine("Pulse 1 para regresar e insetar productos");
+Console.WriteLine("Pulse 2 para actualizar productos");
+var Menu = Convert.ToInt32(Console.ReadLine());
+
+switch (Menu)
+{
+   case 1:
+         int bucle = 1;
+         while(bucle == 1)
+         {
+            Console.WriteLine("Ingresa el nombre de tu producto ");
+            producto.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingresa la descripción de tu producto ");
+            producto.Descipción = Console.ReadLine();
+            Console.WriteLine("Ingresa el precio de tu producto ");
+            producto.Precio = Convert.ToDecimal (Console.ReadLine());
+            Console.WriteLine("Ingresa la cantidad de productos disponibles ");
+            producto.Stock = Convert.ToInt32 (Console.ReadLine());
+         
+            crudProducto.AgregarProducto(producto);
+            Console.WriteLine("El producto se ingreso correctamente");
+            Console.WriteLine("Pulse 1 para continuar añadiendo productos");
+            Console.WriteLine("Pulse 0 para salir");
+
+            bucle = Convert.ToInt32(Console.ReadLine());
+
+         }
+
+   break; 
+   
+   case 2:
+        Console.WriteLine("Actualizar datos");
+   break;
+
+}
